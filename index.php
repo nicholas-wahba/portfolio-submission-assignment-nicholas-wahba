74c1d6a71d301ecbf0575a2d2c911e4c3181143b
nicholas-wahba <nicholaswahba@knights.ucf.edu>
2022-03-04 23:17:21 -0500

    main

--- index.php
+++ index.php
@@ -2,60 +2,68 @@
     <head>
         <title>Psychic Game</title>
             </head>
+    <h2>How To Play:</h2>
+        <p>There are 10 stages and your task is correctly guess what number the "crystal ball" is going to predict!</p>
+        <p>If you guess a number incorrectly in ANY stage, then you'll need to start back to Stage 1!</p>
+        <p>There are 10 stages and your task is correctly guess what number the "crystal ball" is going to predict!</p>
+        <p>If you guess a number incorrectly in ANY stage, then you'll need to start back to Stage 1!</p>
     <body>
+        <br>
         <form action = "index.php" method = "POST">
-            Stage 1: I'm thinking of a number between 1 and 5 <input type = "text" name="Stage1"> <br>
-            Stage 2: I'm thinking of a number between 1 and 10 <input type = "text" name="Stage2"> <br>
-            Stage 3: I'm thinking of a number between 1 and 25 <input type = "text" name="Stage3"> <br>
-            Stage 4: I'm thinking of a number between 1 and 50 <input type = "text" name="Stage4"> <br>
-            Stage 5: I'm thinking of a number between 1 and 100 <input type = "text" name="Stage5"> <br>
-            Stage 6: I'm thinking of a number between 1 and 150 <input type = "text" name="Stage6"> <br>
-            Stage 7: I'm thinking of a number between 1 and 300 <input type = "text" name="Stage7"> <br>
-            Stage 8: I'm thinking of a number between 1 and 500 <input type = "text" name="Stage8"> <br>
-            Stage 9: I'm thinking of a number between 1 and 750 <input type = "text" name="Stage9"> <br>
-            Final Stage: I'm thinking of a number between 1 and 1000 <input type = "text" name="Final"> <br>
+            <b>Stage 1:</b> I'm thinking of a number between 1 and 5 <input type = "radio" name="Stage1"> <br> <br>
+            <b>Stage 2:</b> I'm thinking of a number between 1 and 10 <input type = "radio" name="Stage2"> <br> <br>
+            <b>Stage 3:</b> I'm thinking of a number between 1 and 25 <input type = "radio" name="Stage3"> <br> <br>
+            <b>Stage 4:</b> I'm thinking of a number between 1 and 50 <input type = "radio" name="Stage4"> <br> <br>
+            <b>Stage 5:</b> I'm thinking of a number between 1 and 100 <input type = "radio" name="Stage5"> <br> <br>
+            <b>Stage 6:</b> I'm thinking of a number between 1 and 150 <input type = "radio" name="Stage6"> <br> <br>
+            <b>Stage 7:</b> I'm thinking of a number between 1 and 300 <input type = "radio" name="Stage7"> <br> <br>
+            <b>Stage 8:</b> I'm thinking of a number between 1 and 500 <input type = "radio" name="Stage8"> <br> <br>
+            <b>Stage 9:</b> I'm thinking of a number between 1 and 750 <input type = "radio" name="Stage9"> <br> <br>
+            <b>Final Stage:</b> I'm thinking of a number between 1 and 1000 <input type = "radio" name="Final"> <br> <br>
             <input type = "submit">
         </form>
-    <h1>Crystal Number Ball</h1>
+        <br>
+    <h3>Crystal Ball Predictor</h3>
+        <p>This will general the "crystal ball's" number!</p>
         <?php
             if(isset($_POST["Stage1"])){
-                echo("The number that was chosen was:" . rand(1,5) . "</p>");
+                echo("The number that was chosen was: " . rand(1,5) . "</p>");
             }
 
             if(isset($_POST["Stage2"])){
-                echo("The number that was chosen was:" . rand(1,10) . "</p>");
+                echo("The number that was chosen was: " . rand(1,10) . "</p>");
             }
 
             if(isset($_POST["Stage3"])){
-                echo("The number that was chosen was:" . rand(1,25) . "</p>");
+                echo("The number that was chosen was: " . rand(1,25) . "</p>");
             }
 
             if(isset($_POST["Stage4"])){
-                echo("The number that was chosen was:" . rand(1,50) . "</p>");
+                echo("The number that was chosen was: " . rand(1,50) . "</p>");
             }
 
             if(isset($_POST["Stage5"])){
-                echo("The number that was chosen was:" . rand(1,100) . "</p>");
+                echo("The number that was chosen was: " . rand(1,100) . "</p>");
             }
 
             if(isset($_POST["Stage6"])){
-                echo("The number that was chosen was:" . rand(1,150) . "</p>");
+                echo("The number that was chosen was: " . rand(1,150) . "</p>");
             }
 
             if(isset($_POST["Stage7"])){
-                echo("The number that was chosen was:" . rand(1,300) . "</p>");
+                echo("The number that was chosen was: " . rand(1,300) . "</p>");
             }
 
             if(isset($_POST["Stage8"])){
-                echo("The number that was chosen was:" . rand(1,500) . "</p>");
+                echo("The number that was chosen was: " . rand(1,500) . "</p>");
             }
 
             if(isset($_POST["Stage9"])){
-                echo("The number that was chosen was:" . rand(1,750) . "</p>");
+                echo("The number that was chosen was: " . rand(1,750) . "</p>");
             }
 
             if(isset($_POST["Final"])){
-                echo("The number that was chosen was:" . rand(1,1000) . "</p>");
+                echo("The number that was chosen was: " . rand(1,1000) . "</p>");
             }
         
     ?>
